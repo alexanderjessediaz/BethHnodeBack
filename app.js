53e430c9410b962d2a7a454e5476be5b5da47a13
AJ <alexanderjessediaz@gmail.com>
2020-05-27 11:58:32 -0600

    add sendgrid and axios, emailSenderService route

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
-// HarperDB
-const { HarperDBConnect } = require('harperdb-connect')
-const db = new HarperDBConnect('AJ_Diaz', 'jackie')
-// const authToken = db.setAuthorization('AJ_Diaz', 'jackie')
-
-
-
-
-var server = app.listen(9000, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
 })
 
 
 
-// HarperDB
-db.setDefaultOptions({
-    method: 'POST',
-    headers: {
-            'content-type': 'application/json'
-        },
-        json: true
-    })
-    db.connect('http://localhost:9925')
-    .then(() => {
-        db.request({ operation: "describe_all" })
-          .then(res => console.log(res))
-          .catch(err => console.error(err))
-})
-.catch(err => console.log(err))
 
-// connect to DB function
-// const connectToDB = async () => {
-//     let db;
-//     try {
-//         db = await new HarperDBConnect('Aj_Diaz','Jackie', 'http://localhost:9925')
-//     } catch (err) {
-//         console.log(`Error: ${err}`)
-//     }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+// const { HarperDBConnect } = require('harperdb-connect')
+// const db = new HarperDBConnect('AJ_Diaz', 'jackie')
+
+// // HarperDB
+// db.setDefaultOptions({
+//     method: 'POST',
+//     headers: {
+//             'content-type': 'application/json'
+//         },
+//         json: true
+//     })
+//     db.connect('http://localhost:9925')
+//     .then(() => {
+//         db.request({ operation: "describe_all" })
+//           .then(res => console.log(res))
+//           .catch(err => console.error(err))
+// })
+// .catch(err => console.log(err))
+
+
+
+module.exports = app
 
 
 
@@ -50,13 +55,4 @@ db.setDefaultOptions({
         
         
 
-// CORS
-// const cors = require('cors')
-// app.use(cors())
-// app.get('/products/:id', function (req, res, next) {
-//     res.json({msg: 'This is CORS-enabled for all origins!'})
-//   })
-// app.listen(80, function () {
-//     console.log('CORS-enabled web server listening on port 80')
-//   })
         
--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const express = require('expess')
-const cors = require("cors")
-app.use(cors)
-const bodyParser = require("body-parser")
-const app = express()
+const app = require("./app")
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
-app.use(bodyParser())
 
-const createUser = (req, res) => {
-    res.json({
-        user: "something"
-    })
+const clientEmail = require("./Routes/emailSenderService")
+// var server = app.listen(9000, function () {
+//     var host = server.address().address
+//     var port = server.address().port
+//     console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+app.use("/emailSenderService", clientEmail)
+exports.handler = async (event, context) => {
+    let statusCode = "200"
+
+    if (event.httpMethod === "POST"){
+        const sgMail = require("@sendgrid/mail")
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+        const {email, message } = JSON.parse(event.body)
+        try {
+            await sgMail.send({
+                from: "alexanderjessediaz@gmail.com",
+                to: "alexanderjessediaz@gmail.com",
+                reply_to: email,
+                subject: "Inbound message",
+                text: message,
+            })
+        } catch (err) {
+            statusCode = "500"
+        }
+    }
+    return {
+        statusCode,
+        headers: {
+            "Access-Control-Allow_Origin": "https://localhost:3000",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "POST"
+        },
+    }
 }
 
-
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com',
+//   from: 'test@example.com',
+//   subject: 'Sending with Twilio SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg);
 
 app.listen(PORT, () => console.log(`Listening on... ${PORT}`))
+
