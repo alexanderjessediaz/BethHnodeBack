ff1d3a2b65524d107dfe134a11f9d2d252057058
AJ <alexanderjessediaz@gmail.com>
2020-06-01 17:00:17 -0600

    clean up app.js

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-// app.get('/products/:id', function (req, res, next) {
-//     res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
 
-// app.listen(80, function () {
-//     console.log('CORS-enabled web server listening on port 80')
-// })
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow_Origin","*")
--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const app = require("./app")
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 app.use(cors())
-// app.get('/products/:id', function (req, res, next) {
-//     res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
 
-// app.listen(80, function () {
-//     console.log('CORS-enabled web server listening on port 80')
-// })
-
-
-// const clientEmail = require("./Routes/emailSenderService")
-// app.use("/emailSenderService", clientEmail)
 
 exports.handler = async (event, context) => {
     let statusCode = "200"
@@ -45,22 +35,6 @@ exports.handler = async (event, context) => {
     }
 }
 
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// const msg = {
-    //   to: 'test@example.com',
-    //   from: 'test@example.com',
-    //   subject: 'Sending with Twilio SendGrid is Fun',
-    //   text: 'and easy to do anywhere, even with Node.js',
-    //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    // };
-    // sgMail.send(msg);
-
-    // var server = app.listen(9000, function () {
-    //     var host = server.address().address
-    //     var port = server.address().port
-    //     console.log("Example app listening at http://%s:%s", host, port)
-    // })
     
     app.listen(PORT, () => console.log(`Listening on... ${PORT}`))
     
